e1b13f8d61c9dc0c6de5b184c88b40f63f190125
TatishaS <snow_white_girl@bk.ru>
2022-07-31 04:43:49 +0300

    Main is ready

---
+++ app/js/main.js
@@ -0,0 +1,84 @@
+$(function () {
+  /* Video modal */
+  $('[data-fancybox="video"]').fancybox({
+    buttons: ['zoom', 'fullScreen', 'close'],
+    thumbs: {
+      autoStart: false,
+    },
+  });
+
+  /* Slider */
+  $('.biography__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button style="display: none" class="slick-prev"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><svg width="131" height="27" viewBox="0 0 133 29" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 14.1625H131M131 14.1625L113.378 1M131 14.1625L113.378 28" stroke="white" stroke-width="2"/></svg></button>',
+    /*     responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ], */
+  });
+
+  $('.steps__slider').slick({
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button style="display: none" class="slick-prev"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><svg width="131" height="27" viewBox="0 0 133 29" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 14.1625H131M131 14.1625L113.378 1M131 14.1625L113.378 28" stroke="white" stroke-width="2"/></svg></button>',
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: 'unslick',
+      },
+    ],
+  });
+
+  $('.cases__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button style="display: none" class="slick-prev"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><svg width="131" height="27" viewBox="0 0 133 29" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 14.1625H131M131 14.1625L113.378 1M131 14.1625L113.378 28" stroke="white" stroke-width="2"/></svg></button>',
+  });
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  /* Map modal */
+
+  var init = false;
+
+  $('.fancybox-map').fancybox({
+    buttons: ['close'],
+    //openEffect: 'none',
+    //closeEffect: 'none',
+    beforeLoad: function () {
+      if (init) return;
+      loadMap('map');
+      init = true;
+    },
+    helpers: {
+      title: {
+        type: 'inside',
+        position: 'bottom',
+      },
+      media: {},
+    },
+  });
+
+  function loadMap(target) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src =
+      '//api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A236fab0a399cb25aa9dddb7a9d0915c50f1f7106699eeb8d6b4f38f27fd27098&amp;width=600&amp;height=400&amp;lang=ru_RU&amp;scroll=true&id=' +
+      target;
+    document.head.appendChild(script);
+  }
+});
